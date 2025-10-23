02a8aed575f30ce31d4b7d3df6963e6247ad5be9
RubMurga <ruben.murga.d@gmail.com>
2025-10-23 10:31:19 -0600

    adding docs for auth

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-
+import { serve } from "@hono/node-server"
+import { Hono } from "hono"
+import { auth } from "@/lib/auth"
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app
+  .on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw))
+  .get("/", (c) => {
+    return c.text("Hello Hono!")
+  })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`)
+  }
+)
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import { db } from "@/db"
-
+import { openAPI } from "better-auth/plugins"
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
@@ -9,4 +9,5 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [openAPI()],
 })

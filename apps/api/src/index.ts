e95d0675d7f28e485c23cf3e00739df159206543
RubMurga <ruben.murga.d@gmail.com>
2025-10-23 13:00:19 -0600

    updating index to user env port

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,10 @@
+import "dotenv/config"
 import { serve } from "@hono/node-server"
 import { app } from "@/app"
 serve(
   {
     fetch: app.fetch,
-    port: 3000,
+    port: parseInt(process.env.PORT || "3001"),
   },
   (info) => {
     console.log(`Server is running on http://localhost:${info.port}`)

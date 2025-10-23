bf7d66e852131ae6d5fc3a73ca7ee879fecc8c16
Rubén Murga <RubMurga@users.noreply.github.com>
2025-10-22 23:06:56 -0600

    Configuring Database and better auth (#1)
    * generating config for drizzle and database
    
    * configurin @ for global paths
    
    * adding .env.example
    
    * adding commands for drizzle, creating drizzle config and updating the index for the db connector
    
    * creating migrations for accounts, configuring better auth and improving typescript to import without extensions

---
+++ src/db/index.ts
@@ -0,0 +1,12 @@
+import { drizzle } from "drizzle-orm/node-postgres"
+import { Pool } from "pg"
+import * as schema from "@/db/schema"
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 10,
+})
+
+const db = drizzle(pool, { schema, casing: "snake_case" })
+
+export { db }

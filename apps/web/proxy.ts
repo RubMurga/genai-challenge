d679b57b43b72e40f7db1f2a0237f3d736b1feef
RubMurga <ruben.murga.d@gmail.com>
2025-10-24 16:00:18 -0600

    creating proxy for routes that must have auth

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -1,22 +1,14 @@
-import { headers } from "next/headers"
-import { authClient } from "@/lib/auth"
 import { NextResponse, NextRequest } from "next/server"
+import { getServerSession } from "@/lib/auth-server"
 
 export async function proxy(request: NextRequest) {
-  /*
-  const session = await authClient.api.getSession({
-    headers: await headers(),
-  })
-
+  const session = await getServerSession()
   if (!session) {
     return NextResponse.redirect(new URL("/", request.url))
   }
-  */
-
   return NextResponse.next()
 }
 
 export const config = {
-  runtime: "nodejs",
-  matcher: ["/questions"],
+  matcher: ["/questions", "/profile"],
 }

89fcacc27906c3adf35cc8b838f19b538effe4e9
RubMurga <ruben.murga.d@gmail.com>
2025-10-24 20:47:45 -0600

    creating client

--- apps/web/lib/api.ts
+++ apps/web/lib/api.ts
@@ -1,9 +1,18 @@
 import { hc } from "hono/client"
-import type { AppType } from "@genai/api/src/app"
+import type { AppType } from "@genai/api/dist/app"
 
 const client = hc<AppType>(process.env.NEXT_PUBLIC_SERVER_URL + "/api")
 
 export const getMessages = async () => {
-  const response = await client.api.messages.$get()
+  const response = await client.api.questions.$get()
+  const questions = await response.json()
+  return questions
+}
+
+export const createMessage = async (content: string) => {
+  const response = await client.api.questions.$post({
+    json: { content },
+  })
+
   return response.json()
 }
